06cfd7ed449aa22a4d69aea2eff6fa2d878c1852
Adebayo Rilwam Ajibola <ajibolarilwan14@gmail.com>
2021-01-07 15:02:08 +0100

    initial commit

---
+++ indecision-app/public/scripts/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'i just change something!'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+render();
---
+++ indecision-app/src/app.js
@@ -0,0 +1,5 @@
+var template = <p>i just change something!</p>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot)
